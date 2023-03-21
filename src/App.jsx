9b95df64fcb30b5e5b0289724dbe44deaf267f21
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-21 20:34:44 +0100

    imported product details, cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useThemeHook } from "./GlobalComponents/ThemeProvider";
 import Header from "./Components/Header";
-// import { Router } from "@reach/router";
+import { Router } from "@reach/router";
+
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
+import ProductDetails from "./Pages/ProductDetails";
 
 function App() {
   const [theme] = useThemeHook();
@@ -13,6 +17,14 @@ function App() {
       style={{ height: "100vh", overflowY: "auto" }}
     >
       <Header />
+      <Router>
+        <Home path="/src/Pages/Home.jsx" />
+        {/* <MyAccount path="my-account" /> */}
+        {/* <SignIn path="sign-in" /> */}
+        {/* <Register path="register" /> */}
+        <ProductDetails path="product-details/:productId" />
+        <Cart path="/src/Pages/Cart.jsx" />
+      </Router>
     </main>
   );
 }
