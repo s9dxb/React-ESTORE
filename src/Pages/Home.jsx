2660bb721c89fc229823c34b509c99277b9f945d
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-21 20:33:22 +0100

    added home details

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import { useThemeHook } from "../GlobalComponents/ThemeProvider";
+import { BiSearch } from "react-icons/bi";
+import SearchFilter from "react-filter-search";
+import ProductCard from "../Components/ProductCard";
 
-export const Home = () => {
+const Home = () => {
   const [theme] = useThemeHook();
   const [searchInput, setSearchInput] = useState("");
-  const [productData, setProductData] = useDtate("");
+  const [productData, setProductData] = useState([]);
 
   async function getResponse() {
     const res = await fetch("https://fakestoreapi.com/products").then((res) =>
@@ -16,5 +21,46 @@ export const Home = () => {
     getResponse();
   }, []);
 
-  return <Container></Container>;
+  return (
+    <Container className="py-4">
+      <Row className="justify-content-center">
+        <Col xs={10} md={7} lg={6} xl={4} className="mb-3 mx-auto text-center">
+          <h1 className={theme ? "text-light my-5" : "text-black my-5"}>
+            Search products
+          </h1>
+          <InputGroup className="mb-3">
+            <InputGroup.Text
+              className={
+                theme
+                  ? "bg-black text-dark-primary"
+                  : "bg-light text-light-primary"
+              }
+            >
+              <BiSearch size="2rem" />
+            </InputGroup.Text>
+            <FormControl
+              placeholder="Search"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className={
+                theme ? "bg-light-black text-light" : "bg-light text-black"
+              }
+            />
+          </InputGroup>
+        </Col>
+        <SearchFilter
+          value={searchInput}
+          data={productData}
+          renderResults={(results) => (
+            <Row className="justify-content-center">
+              {results.map((item, i) => (
+                <ProductCard data={item} key={i} />
+              ))}
+            </Row>
+          )}
+        />
+      </Row>
+    </Container>
+  );
 };
+export default Home;
