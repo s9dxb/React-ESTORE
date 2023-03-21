f348df9c75188d41ba5472ec530e007de15ffe15
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-21 22:18:33 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useThemeHook } from "./GlobalComponents/ThemeProvider";
 import Header from "./Components/Header";
-import { Router } from "@reach/router";
+// import { Router } from "@reach/router";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
@@ -17,14 +18,14 @@ function App() {
       style={{ height: "100vh", overflowY: "auto" }}
     >
       <Header />
-      <Router>
-        <Home path="/src/Pages/Home.jsx" />
+      <Routes>
+        <Route path="/src/Pages/Home.jsx" element={<Home />} />
         {/* <MyAccount path="my-account" /> */}
         {/* <SignIn path="sign-in" /> */}
         {/* <Register path="register" /> */}
-        <ProductDetails path="product-details/:productId" />
-        <Cart path="/src/Pages/Cart.jsx" />
-      </Router>
+        <Route path="product-details/:productId" element={<ProductDetails />} />
+        <Route path="/Cart" element={<Cart />} />
+      </Routes>
     </main>
   );
 }
