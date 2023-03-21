424da4fd8415b74ebb89da18f5fe6016abf71589
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-21 15:24:58 +0100

    imported header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useThemeHook } from "./GlobalComponents/ThemeProvider";
+import Header from "./Components/Header";
+// import { Router } from "@reach/router";
 
 function App() {
-  return <div className="App"></div>;
+  const [theme] = useThemeHook();
+  return (
+    <main
+      className={theme ? "bg-black" : "bg-light-2"}
+      style={{ height: "100vh", overflowY: "auto" }}
+    >
+      <Header />
+    </main>
+  );
 }
 
 export default App;
