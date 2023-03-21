6f5281a4368019fe21cb6942ed84ccfec4dafd05
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-21 20:35:14 +0100

    installed dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { render } from "react-dom";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 // import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./GlobalComponents/ThemeProvider";
 import { CartProvider } from "react-use-cart";
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <CartProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </CartProvider>
     </ThemeProvider>
   </React.StrictMode>
