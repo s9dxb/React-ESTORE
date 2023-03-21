f397e2ce3ecd79592693ac27a1dc08bdb39571be
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-21 15:25:20 +0100

    added theme provider, cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
+import App from "./App";
+import "./index.css";
+// import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "./GlobalComponents/ThemeProvider";
+import { CartProvider } from "react-use-cart";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
